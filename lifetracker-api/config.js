60f1359851ef1f112dacf6d437ff99d15df78ccb
Camila Naranjo <cnaranjo@cnaranj-ltm6e77.internal.salesforce.com>
2023-06-29 10:27:23 -0700

    refactored chaiapies

--- lifetracker-api/config.js
+++ lifetracker-api/config.js
@@ -1,30 +1,66 @@
-require('dotenv').config();
+const dotenv = require('dotenv');
+dotenv.config();
 
-const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 5000
-const IS_TESTING = process.env.NODE_ENV === "test" 
-const SECRET_KEY = process.env.SECRET_KEY || "secret"
-const BCRYPT_WORK_FACTOR = process.env.BCRYPT_WORK_FACTOR || 13
+const SERVER_PORT = process.env.SERVER_PORT || 5000;
+const SECRET_KEY = process.env.SECRET_KEY || 'fvaeraeaebdbeberbawbw';
+const BCRYPT_WORK_FACTOR = process.env.BCRYPT_WORK_FACTOR || 13;
 
-// Use dev database, testing database, or via env var, production database
 function getDatabaseUri() {
-  const DATABASE_USER = process.env.DATABASE_USER || "me"
-  const DATABASE_PASS = process.env.DATABASE_PASS ? encodeURI(process.env.DATABASE_PASS) : "postgres"
-  const DATABASE_HOST = process.env.DATABASE_HOST || "localhost"
-  const DATABASE_PORT = process.env.DATABASE_PORT || 5432
-  const DATABASE_TEST_NAME = process.env.DATABASE_TEST_NAME || "lifetracker_test"
-  const DATABASE_NAME = process.env.DATABASE_NAME || "lifetracker"
-  const BCRYPT_WORK_FACTOR = process.env.BCRYPT_WORK_FACTOR || 13
-  const IS_TESTING = process.env.NODE_ENV === "test"
-  return (IS_TESTING)
-  ? 'postgresql://me:postgres@localhost:localhost/lifetracker_test'
-  : process.env.DATABASE_URL || `postgresql://${DATABASE_USER}:${DATABASE_PASS}@${DATABASE_HOST}:${DATABASE_HOST}/${DATABASE_NAME}`
-}
+  const DATABASE_USER = process.env.DATABASE_USER || 'me';
+  const DATABASE_PASS = process.env.DATABASE_PASS ? encodeURI(process.env.DATABASE_PASS) : 'postgres';
+  const DATABASE_HOST = process.env.DATABASE_HOST || 'localhost';
+  const DATABASE_PORT = process.env.DATABASE_PORT || 5432;
+  const DATABASE_TEST_NAME = process.env.DATABASE_TEST_NAME || 'lifetracker_test';
+  const DATABASE_NAME = process.env.DATABASE_NAME || 'lifetracker';
+  const BCRYPT_WORK_FACTOR = process.env.BCRYPT_WORK_FACTOR || 13;
+  const IS_TESTING = process.env.IS_TESTING === 'true';
 
+  if (IS_TESTING) {
+    return {
+      connectionString: `postgresql://${DATABASE_USER}:${DATABASE_PASS}@${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_TEST_NAME}`,
+    };
+  }
+
+  if (process.env.DATABASE_URL !== undefined) {
+    return {
+      connectionString: process.env.DATABASE_URL
+    };
+  }
+  return {
+    connectionString: `postgresql://${DATABASE_USER}:${DATABASE_PASS}@${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}`,
+  };
+}
 
 module.exports = {
   SERVER_PORT,
-  IS_TESTING,
   getDatabaseUri,
   BCRYPT_WORK_FACTOR,
-  SECRET_KEY
+  SECRET_KEY,
 };
+
+
+// Use dev database, testing database, or via env var, production database
+// function getDatabaseUri() {
+//   const DATABASE_USER = process.env.DATABASE_USER || "me"
+//   const DATABASE_PASS = process.env.DATABASE_PASS ? encodeURI(process.env.DATABASE_PASS) : "postgres"
+//   const DATABASE_HOST = process.env.DATABASE_HOST || "localhost"
+//   const DATABASE_PORT = process.env.DATABASE_PORT || 5432
+//   const DATABASE_TEST_NAME = process.env.DATABASE_TEST_NAME || "lifetracker_test"
+//   const DATABASE_NAME = process.env.DATABASE_NAME || "lifetracker"
+//   const BCRYPT_WORK_FACTOR = process.env.BCRYPT_WORK_FACTOR || 13
+//   const IS_TESTING = process.env.NODE_ENV === "test"
+//   return (IS_TESTING)
+//   ? 'postgresql://me:postgres@localhost:localhost/lifetracker_test'
+//   : process.env.DATABASE_URL || `postgresql://${DATABASE_USER}:${DATABASE_PASS}@${DATABASE_HOST}:${DATABASE_HOST}/${DATABASE_NAME}`
+// }
+
+
+// module.exports = {
+//   SERVER_PORT,
+//   IS_TESTING,
+//   getDatabaseUri,
+//   BCRYPT_WORK_FACTOR,
+//   SECRET_KEY
+// };
+
+
--- lifetracker-api/config.test.js
+++ lifetracker-api/config.test.js
@@ -1,82 +1,69 @@
-process.env.NODE_ENV = 'test';
-process.env.IS_TESTING = 'true';
-process.env.SERVER_PORT = 5000; 
-process.env.SECRET_KEY = 'fvaeraeaebdbeberbawbw'; 
-process.env.BCRYPT_WORK_FACTOR = 13; 
-
-const assert = require('chai').assert;
+const { expect } = require('chai');
+const chai = require('chai');
+const spies = require('chai-spies');
 const config = require('./config.js');
-config.getDatabaseUri();
+
+chai.use(spies);
 
 describe('Env Variables', function() {
+  
+  before(function() {
+    process.env.NODE_ENV = 'test';
+    process.env.SERVER_PORT = 5000;
+    process.env.SECRET_KEY = 'fvaeraeaebdbeberbawbw';
+    process.env.BCRYPT_WORK_FACTOR = 13;
+    config.IS_TESTING = process.env.NODE_ENV === 'test';
+  });
+  
+
   describe('process.env.NODE_ENV', function() {
     it('should be set to "test" when the test suite is run', function() {
-      assert.strictEqual(process.env.NODE_ENV, 'test');
+      expect(process.env.NODE_ENV).to.equal('test');
     });
   });
 
   describe('IS_TESTING variable', function() {
     it('should be exported and only true if process.env.NODE_ENV is "test"', function() {
-      assert.strictEqual(config.IS_TESTING, process.env.NODE_ENV === 'test');
+      expect(config.IS_TESTING).to.equal(true);
     });
   });
 
   describe('Env variables in config.js', function() {
-    it('should export PORT', function() {
-      assert.exists(config.SERVER_PORT);
-    });
+    const expectedVariables = ['SERVER_PORT', 'SECRET_KEY', 'BCRYPT_WORK_FACTOR'];
 
-    it('should export SECRET_KEY', function() {
-      assert.exists(config.SECRET_KEY);
-    });
-
-    it('should export BCRYPT_WORK_FACTOR', function() {
-      assert.exists(config.BCRYPT_WORK_FACTOR);
-    });
-
-    it('should export IS_TESTING', function() {
-      assert.exists(config.IS_TESTING);
+    expectedVariables.forEach(variable => {
+      it(`should export ${variable}`, function() {
+        expect(config[variable]).to.exist;
+      });
     });
   });
 
   describe('getDatabaseUri function', function() {
     it('should be exported from the config.js file', function() {
-      assert.isFunction(config.getDatabaseUri);
+      expect(config.getDatabaseUri).to.be.a('function');
     });
 
     it('should return process.env.DATABASE_URL if it exists', function() {
-      process.env.DATABASE_URL = 'test-database-uri';
-      assert('test-database-url'.localeCompare(config.getDatabaseUri()),config.getDatabaseUri() === 'test-database-url')
-      delete process.env.DATABASE_URL; 
+      const databaseUrl = 'test-database-url';
+      process.env.DATABASE_URL = databaseUrl;
+      expect(config.getDatabaseUri().connectionString).to.equal(databaseUrl);
+      delete process.env.DATABASE_URL;
     });
 
     it('should use the test database when IS_TESTING is true', function() {
-      process.env.IS_TESTING = true; 
-      assert.strictEqual(config.getDatabaseUri(), 'postgresql://me:postgres@localhost:localhost/lifetracker_test');
+      const expectedConnectionString = 'postgresql://me:postgres@localhost:5432/lifetracker_test';
+      process.env.IS_TESTING = true;
+      expect(config.getDatabaseUri().connectionString).to.equal(expectedConnectionString);
     });
 
     it('should combine the proper database environment variables if DATABASE_URL does not exist', function() {
-      delete process.env.DATABASE_URL; // Ensure DATABASE_URL is not set
-      delete process.env.IS_TESTING; // Ensure IS_TESTING is not set
-      delete process.env.NODE_ENV; // Ensure NODE_ENV is not set
+      delete process.env.DATABASE_URL;
+      delete process.env.IS_TESTING;
+      delete process.env.NODE_ENV;
 
-      process.env.DATABASE_HOST = 'localhost';
-      process.env.DATABASE_PORT = 5432;
-      process.env.DATABASE_NAME = 'lifetracker';
-      process.env.DATABASE_USER = 'me';
-      process.env.DATABASE_PASS = 'postgres';
-
-      assert.strictEqual(
-        config.getDatabaseUri(),
-        "postgresql://me:postgres@localhost:localhost/lifetracker"
-         );
-      
-      // Clean up the environment variables after the test
-      delete process.env.DATABASE_HOST; 
-      delete process.env.DATABASE_PORT;
-      delete process.env.DATABASE_NAME;
-      delete process.env.DATABASE_USER;
-      delete process.env.DATABASE_PASS;
+      const expectedConnectionString = 'postgresql://me:postgres@localhost:5432/lifetracker';
+      expect(config.getDatabaseUri().connectionString).to.equal(expectedConnectionString);
     });
   });
 });
+
